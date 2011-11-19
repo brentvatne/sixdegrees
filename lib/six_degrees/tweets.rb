cdedefddd1524792d07f76111b17138bc2676631
Brent Vatne <brentvatne@gmail.com>
2011-11-19 14:06:44 -0500

    tweet parsing passing specs

--- lib/six_degrees/tweets.rb
+++ lib/six_degrees/tweets.rb
@@ -1,3 +1,6 @@
+# Provides StringScanner to perform parsing
+require 'strscan'
+
 # Packages the parse method and User class
 # The only public method that can be called on Tweets
 # is parse, which is a class method
@@ -15,25 +18,54 @@ class Tweets
   #   # => a collection of User objects
   #
   # Returns a collection of User objects
-  def self.parse(file)
-    [User.new]
+  class << self
+    def parse(tweets)
+      users = Hash.new { |hash, name| hash[name] = User.new(name) }
+
+      each_tweet(tweets) do |t|
+        name     = parse_username(t)
+        mentions = parse_mentions(t)
+
+        users[name].add_mentions(mentions)
+      end
+
+      users.values
+    end
+
+    def each_tweet(tweets)
+      tweets.split(/\\n/).each do |tweet|
+        yield(tweet)
+      end
+    end
+
+    def parse_username(tweet)
+      tweet.match(/^\w+/).to_a.first
+    end
+
+    def parse_mentions(tweet)
+      tweet.scan(/@\w+/).to_a.map do |m|
+        m.gsub!("@","")
+      end
+    end
   end
+
 
   class User
     attr_accessor :name, :mentions
 
     def initialize(name="alberta")
       self.name = name
-      self.mentions = ["bob"]
+      self.mentions = []
     end
 
-    def add_mention(name)
-      @mention ||= []
-      @mention << name
+    # Private: Merges a collection of names to existing mentions list
+    # using set union to guarantee uniqueness of items
+    def add_mentions(names)
+      @mentions = @mentions | names
     end
 
     def mentioned?(name)
-      @mention.include?(name)
+      @mentions.include?(name)
     end
   end
 
--- spec/tweets_spec.rb
+++ spec/tweets_spec.rb
@@ -1,11 +1,23 @@
 describe Tweets do
 
-  context "Tweets.parse" do
-    let(:tweet_file) { StringIO.new('alberta: @bob "It is remarkable,
-                       the character of the pleasure we derive from
-                       the best books.\n') }
-    let(:parsed_users) { Tweets.parse(tweet_file) }
-    let(:return_value) { parsed_users }
+  let(:tweet) { 'alberta: @bob "It is remarkable, the character of the pleasure we derive from the best books. /cc @christie\n' }
+  let(:multiple_tweets) { tweet + 'bob: "They impress us ever with the conviction that one nature wrote and the same reads." /cc @alberta\n' }
+
+  describe "Tweets.parse_name" do
+    it "should parse the user name" do
+      Tweets.parse_username(tweet).should == "alberta"
+    end
+  end
+
+  describe "Tweets.parse_mentions" do
+    it "should parse the mentions" do
+      Tweets.parse_mentions(tweet).should == ["bob", "christie"]
+    end
+  end
+
+  describe "Tweets.parse" do
+    let(:parsed_users)    { Tweets.parse(tweet) }
+    let(:return_value)    { parsed_users }
 
     it "returns a collection" do
       return_value.should respond_to(:[])
@@ -24,16 +36,26 @@ describe Tweets do
     it "correctly parses the users mentioned in a tweet" do
       parsed_users.first.mentions.should include("bob")
     end
+
+    it "parses more than one user" do
+      users = Tweets.parse(multiple_tweets)
+      first_user, second_user = users.first(2)
+
+      first_user.name.should == "alberta"
+      second_user.name.should == "bob"
+
+      first_user.mentions.should include("bob")
+      first_user.mentions.should include("christie")
+      second_user.mentions.should include("alberta")
+    end
   end
 
   context "Tweets::User" do
-
     describe "mentioned?" do
       subject { Tweets::User.new("alberta") }
 
       before do
-        subject.add_mention("bob")
-        subject.add_mention("brent")
+        subject.add_mentions(["bob", "brent"])
       end
 
       it "returns true if the subject has been mentioned the given name" do
@@ -44,6 +66,5 @@ describe Tweets do
         subject.mentioned?("jane").should be_false
       end
     end
-
   end
 end
