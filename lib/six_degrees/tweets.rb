5ae14d8e190e1a586ea006a31ec7fd8bfb18300d
Brent Vatne <brentvatne@gmail.com>
2011-11-19 14:59:27 -0500

    tweak comments

--- lib/six_degrees/tweets.rb
+++ lib/six_degrees/tweets.rb
@@ -1,72 +1,112 @@
-# Provides StringScanner to perform parsing
-require 'strscan'
+module SixDegrees
+  # Packages methods for parsing a string containing multiple tweets
+  class Twitter
 
-# Packages the parse method and User class
-# The only public method that can be called on Tweets
-# is parse, which is a class method
+    # Public: Parse a string of text containing multiple tweets to
+    # extract usernames, both of the user who tweeted and users
+    # mentioned within tweets.
+    #
+    # file - The string to be parsed. If using a file, you must read it
+    # first into a string and then pass that string in.
+    #
+    # Examples:
+    #
+    #   tweets = File.open("tweets.txt").read
+    #   users = Twitter.parse(tweets)
+    #   # => a collection of User objects
+    #
+    # Returns a collection of User objects
+    class << self
+      def parse(tweets)
+        users = Hash.new { |hash, name| hash[name] = User.new(name) }
 
-class Tweets
+        each_tweet(tweets) do |t|
+          name     = parse_username(t)
+          mentions = parse_mentions(t)
 
-  # Public: Parse a text buffer of tweets into users and mentions
-  #
-  # file - The buffer to be parsed, can be a File object or StringIO
-  #
-  # Examples:
-  #
-  #   tweets_file = File.open("tweets.txt")
-  #   users = Tweets.parse(tweets_file)
-  #   # => a collection of User objects
-  #
-  # Returns a collection of User objects
-  class << self
-    def parse(tweets)
-      users = Hash.new { |hash, name| hash[name] = User.new(name) }
+          users[name].add_mentions(mentions)
+        end
 
-      each_tweet(tweets) do |t|
-        name     = parse_username(t)
-        mentions = parse_mentions(t)
-
-        users[name].add_mentions(mentions)
+        users.values
       end
 
-      users.values
-    end
+      # Private: Yield a single tweet to a passed block
+      #
+      # tweets - A string containing multiple tweets separated by
+      # newlines
+      #
+      # Abstracts the procedure of separating a list into individual
+      # tweets
+      #
+      # No meaningful return value
+      def each_tweet(tweets)
+        tweets.split(/\\n/).each do |tweet|
+          yield(tweet)
+        end
+      end
 
-    def each_tweet(tweets)
-      tweets.split(/\\n/).each do |tweet|
-        yield(tweet)
+      # Private: Finds the name of the user who made a given tweet
+      #
+      # tweet - A string containing a single tweet
+      #
+      # Returns a string with the name of the user, eg: "notbrent"
+      def parse_username(tweet)
+        tweet.match(/^\w+/).to_a.first
+      end
+
+      # Private Finds the names of every mentioned user within a given
+      # tweet
+      #
+      # tweet - A string containing a single tweet
+      #
+      # Returns an array with the names of each user
+      def parse_mentions(tweet)
+        tweet.scan(/@\w+/).to_a.map do |m|
+          m.gsub!("@","")
+        end
       end
     end
 
-    def parse_username(tweet)
-      tweet.match(/^\w+/).to_a.first
-    end
+    # Encapsulates the concept of a User that has a name and mentions
+    # other users. A convenience class to make interacting with this
+    # data easier and in appropriate domain language.
+    class User
+      attr_reader :name, :mentions
 
-    def parse_mentions(tweet)
-      tweet.scan(/@\w+/).to_a.map do |m|
-        m.gsub!("@","")
+      def initialize(name)
+        @name = name
+        @mentions = []
+      end
+
+      # Public: Determines whether the user has mentioned another user
+      #
+      # name - A string containing the name of the user
+      #
+      # Returns true if yes, false if no
+      def mentioned?(name)
+        mentions.include?(name)
+      end
+
+      # Public: Performs validation before adding a name to the list
+      #
+      # name - A string containing the name of the user
+      #
+      # Returns a reference to the mentions array
+      def add_mention(name)
+        @mentions << name unless name == self.name or mentions.include?(name)
+        mentions
+      end
+
+      # Public: Accepts a collection of names and proxies them one by
+      # one to the add_mention method
+      #
+      # names - A collection of strings containing name of mentioned users
+      #
+      # Returns a reference to the mentions array
+      def add_mentions(names)
+        names.each { |name| add_mention(name) }
+        mentions
       end
     end
   end
-
-
-  class User
-    attr_accessor :name, :mentions
-
-    def initialize(name="alberta")
-      self.name = name
-      self.mentions = []
-    end
-
-    # Private: Merges a collection of names to existing mentions list
-    # using set union to guarantee uniqueness of items
-    def add_mentions(names)
-      @mentions = @mentions | names
-    end
-
-    def mentioned?(name)
-      @mentions.include?(name)
-    end
-  end
-
 end
--- spec/tweets_spec.rb
+++ spec/tweets_spec.rb
@@ -1,32 +1,25 @@
-describe Tweets do
-
+describe SixDegrees::Twitter do
   let(:tweet) { 'alberta: @bob "It is remarkable, the character of the pleasure we derive from the best books. /cc @christie\n' }
   let(:multiple_tweets) { tweet + 'bob: "They impress us ever with the conviction that one nature wrote and the same reads." /cc @alberta\n' }
 
-  describe "Tweets.parse_name" do
-    it "should parse the user name" do
-      Tweets.parse_username(tweet).should == "alberta"
+  describe "self#parse_name" do
+    it "should parse the user name in a single tweet" do
+      SixDegrees::Twitter.parse_username(tweet).should == "alberta"
     end
   end
 
-  describe "Tweets.parse_mentions" do
-    it "should parse the mentions" do
-      Tweets.parse_mentions(tweet).should == ["bob", "christie"]
+  describe "self#parse_mentions" do
+    it "should parse the mentions in a single tweet" do
+      SixDegrees::Twitter.parse_mentions(tweet).should == ["bob", "christie"]
     end
   end
 
-  describe "Tweets.parse" do
-    let(:parsed_users)    { Tweets.parse(tweet) }
+  describe "self#parse" do
+    let(:parsed_users)    { SixDegrees::Twitter.parse(tweet) }
     let(:return_value)    { parsed_users }
 
     it "returns a collection" do
       return_value.should respond_to(:[])
-    end
-
-    it "populates the returned collection with Tweets::User objects" do
-      return_value.each do |item|
-        item.should be_kind_of Tweets::User
-      end
     end
 
     it "correctly parses the name of the user in a tweet" do
@@ -38,7 +31,7 @@ describe Tweets do
     end
 
     it "parses more than one user" do
-      users = Tweets.parse(multiple_tweets)
+      users = SixDegrees::Twitter.parse(multiple_tweets)
       first_user, second_user = users.first(2)
 
       first_user.name.should == "alberta"
@@ -50,9 +43,9 @@ describe Tweets do
     end
   end
 
-  context "Tweets::User" do
+  context "SixDegrees::Twitter::User" do
     describe "mentioned?" do
-      subject { Tweets::User.new("alberta") }
+      subject { SixDegrees::Twitter::User.new("alberta") }
 
       before do
         subject.add_mentions(["bob", "brent"])
@@ -64,6 +57,23 @@ describe Tweets do
 
       it "returns false if the subject has been mentioned the given name" do
         subject.mentioned?("jane").should be_false
+      end
+    end
+
+    describe "add_mention" do
+      subject { SixDegrees::Twitter::User.new("brent") }
+
+      describe "validations" do
+        it "should not add the user's own name to their mentions" do
+          subject.add_mention("brent")
+          subject.mentions.should be_empty
+        end
+
+        it "should not add a user's name if they are already mentioned" do
+          after_first_mention  = subject.add_mention("ana maria")
+          after_second_mention = subject.add_mention("ana ma")
+          after_first_mention.should == after_second_mention
+        end
       end
     end
   end
