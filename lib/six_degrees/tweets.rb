193c32bc834d3f08afb1ce1fc596a280994e713d
Brent Vatne <brentvatne@gmail.com>
2011-11-19 12:37:59 -0500

    realize the base wasn't quite there yet, adda few more tests, now ready to be broken to make parser

--- lib/six_degrees/tweets.rb
+++ lib/six_degrees/tweets.rb
@@ -1,11 +1,40 @@
+# Packages the parse method and User class
+# The only public method that can be called on Tweets
+# is parse, which is a class method
+
 class Tweets
 
+  # Public: Parse a text buffer of tweets into users and mentions
+  #
+  # file - The buffer to be parsed, can be a File object or StringIO
+  #
+  # Examples:
+  #
+  #   tweets_file = File.open("tweets.txt")
+  #   users = Tweets.parse(tweets_file)
+  #   # => a collection of User objects
+  #
+  # Returns a collection of User objects
   def self.parse(file)
     [User.new]
   end
 
   class User
+    attr_accessor :name, :mentions
 
+    def initialize(name="alberta")
+      self.name = name
+      self.mentions = ["bob"]
+    end
+
+    def add_mention(name)
+      @mention ||= []
+      @mention << name
+    end
+
+    def mentioned?(name)
+      @mention.include?(name)
+    end
   end
 
 end
--- spec/tweets_spec.rb
+++ spec/tweets_spec.rb
@@ -1,31 +1,49 @@
 describe Tweets do
 
   context "Tweets.parse" do
-
-    let(:tweet_file) {
-      StringIO.new('alberta: @bob "It is remarkable, the character of the 
-                    pleasure we derive from the best books.\n') }
-    let(:parsed) { Tweets.parse(tweet_file) }
-    let(:return_value) { parsed }
+    let(:tweet_file) { StringIO.new('alberta: @bob "It is remarkable,
+                       the character of the pleasure we derive from
+                       the best books.\n') }
+    let(:parsed_users) { Tweets.parse(tweet_file) }
+    let(:return_value) { parsed_users }
 
     it "returns a collection" do
       return_value.should respond_to(:[])
     end
 
-    it "populates the returned collection with TwitterUser objects" do
+    it "populates the returned collection with Tweets::User objects" do
       return_value.each do |item|
         item.should be_kind_of Tweets::User
       end
     end
 
-    # it "correctly parses the name of the user in a tweet" do
-    #   parsed_users.first.name.should == "alberta"
-    # end
+    it "correctly parses the name of the user in a tweet" do
+      parsed_users.first.name.should == "alberta"
+    end
 
-    # it "correctly parses the users mentioned in a tweet" do
-    #   parsed_users.first.mentions.should == ["bob"]
-    # end
-
+    it "correctly parses the users mentioned in a tweet" do
+      parsed_users.first.mentions.should include("bob")
+    end
   end
 
+  context "Tweets::User" do
+
+    describe "mentioned?" do
+      subject { Tweets::User.new("alberta") }
+
+      before do
+        subject.add_mention("bob")
+        subject.add_mention("brent")
+      end
+
+      it "returns true if the subject has been mentioned the given name" do
+        subject.mentioned?("bob").should be_true
+      end
+
+      it "returns false if the subject has been mentioned the given name" do
+        subject.mentioned?("jane").should be_false
+      end
+    end
+
+  end
 end
